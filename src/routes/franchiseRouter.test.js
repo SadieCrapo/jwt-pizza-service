3f48d06d43c6f4b1f3a8a0a50fdeb10e759f1c03
Sadie Crapo <scrapo5@byu.edu>
2026-02-05 15:40:46 -0700

    Implement test for creating a franchise

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const app = require('../service');
+
+const adminUser = { name: "admin user", email: "user@admin.com", password: "toomanysecrets" };
+let adminUserAuthToken;
+const newFranchise = { admins: [], name: "newest franchise", stores: [] };
+const { Role, DB } = require('../database/database.js');
+
+beforeAll(async () => {
+    await createAdminUser();
+
+    const loginRes = await request(app).put('/api/auth').send(adminUser);
+
+    expect(loginRes.status).toBe(200);
+    adminUserAuthToken = loginRes.body.token;
+});
+
+async function createAdminUser() {
+  adminUser.name = Math.random().toString(36).substring(2, 12);
+  adminUser.email = adminUser.name + '@admin.com';
+  adminUser.roles = [{ role: Role.Admin }];
+
+  user = await DB.addUser(adminUser);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+test('create franchise', async () => {
+    newFranchise.name = Math.random().toString(36).substring(2, 12);
+    const createRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newFranchise);
+
+    console.log(createRes.body);
+    expect(createRes.status).toBe(200);
+    expect(createRes.body).toMatchObject(newFranchise);
+});
