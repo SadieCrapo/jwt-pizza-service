8fd5059106d0f474d8bff3f2821b17fc535eb2c0
Sadie Crapo <scrapo5@byu.edu>
2026-02-09 13:07:10 -0700

    Write service test for creating a new order

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -6,7 +6,10 @@ const normalUser = { name: "normal user", email: "user@test.com", password: "a" 
 let adminUserAuthToken;
 let normalUserAuthToken;
 const newItem = { title: "new item", description: "the newest item", image: "some image url", price: 12.46 };
+const newFranchise = { admins: [], name: "newest franchise", stores: [] };
 const newStore = { name: "store number one" }
+const newOrder = { franchiseId: "franchise number one", storeId: "store number one", date: "today", items: [] };
+
 const { Role, DB } = require('../database/database.js');
 
 beforeAll(async () => {
@@ -39,6 +42,29 @@ async function addMenuItem() {
     return newItem;
 }
 
+async function createFranchise() {
+    newFranchise.name = Math.random().toString(36).substring(2, 12);
+    const createRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newFranchise);
+
+    expect(createRes.status).toBe(200);
+    expect(createRes.body).toMatchObject(newFranchise);
+    return createRes.body.id;
+}
+
+async function createStore(franchiseId) {
+    newStore.name = Math.random().toString(36).substring(2, 12);
+
+    const createRes = await request(app).post(`/api/franchise/${franchiseId}/store`).set('Authorization', `Bearer ${adminUserAuthToken}`).send(newStore);
+
+    expect(createRes.status).toBe(200);
+    expect(createRes.body).toMatchObject(newStore);
+    return createRes.body.id;
+}
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
 test('add menu item', addMenuItem);
 
 test('add item exception', async () => {
@@ -56,4 +82,23 @@ test('get menu', async () => {
     const getRes = await request(app).get('/api/order/menu');
     expect(getRes.status).toBe(200);
     expect(getRes.body).toEqual(expect.arrayContaining([expect.objectContaining(expectedMenu[0]), expect.objectContaining(expectedMenu[1]), expect.objectContaining(expectedMenu[2])]));
+});
+
+test('create order', async () => {
+    const franchiseId = await createFranchise();
+    const storeId = await createStore(franchiseId);
+
+    await addMenuItem();
+    let menu = await request(app).get('/api/order/menu');
+
+    let newOrderItem = { menuId: menu.body[0].id, description: menu.body[0].description, price: menu.body[0].price };
+
+    newOrder.franchiseId = franchiseId;
+    newOrder.storeId = storeId;
+    newOrder.items = [newOrderItem];
+    const createRes = await request(app).post('/api/order').set('Authorization', `Bearer ${normalUserAuthToken}`).send(newOrder);
+
+    expect(createRes.status).toBe(200);
+    expect(createRes.body.order).toMatchObject(newOrder);
+    expectValidJwt(createRes.body.jwt);
 });
